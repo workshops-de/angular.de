6b6458c5f9b9e4b3682590e0e81782daffe08d42
Martin Schurig <martin@schurig.pw>
2017-04-26 15:51:17 +0200

    print out path to file that includes tabs

--- _plugins/tab_banner.rb
+++ _plugins/tab_banner.rb
@@ -1,13 +1,15 @@
 Jekyll::Hooks.register :posts, :pre_render do |p|
   if p.content.include?("\t")
-    puts "\n\n!!! Please don't use tabs !!!"
+    puts "\n\n# Please don't use tabs!!!"
+    puts p.path
     exit 1
   end
 end
 
 Jekyll::Hooks.register :pages, :pre_render do |p|
   if p.content.include?("\t")
-    puts "\n\n!!! Please don't use tabs !!!"
+    puts "\n\n# Please don't use tabs!!!"
+    puts p.path
     exit 1
   end
 end
