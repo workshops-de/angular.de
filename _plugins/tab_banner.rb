62da68df52a0d5d09b52823e6b8af94ff339e665
Martin Schurig <martin@schurig.pw>
2017-04-26 15:43:06 +0200

    check for tabs

---
+++ _plugins/tab_banner.rb
@@ -0,0 +1,13 @@
+Jekyll::Hooks.register :posts, :pre_render do |p|
+  if p.content.include?('\t')
+    puts "\n\n!!! Please don't use tabs !!!"
+    exit 1
+  end
+end
+
+Jekyll::Hooks.register :pages, :pre_render do |p|
+  if p.content.include?('\t')
+    puts "\n\n!!! Please don't use tabs !!!"
+    exit 1
+  end
+end
