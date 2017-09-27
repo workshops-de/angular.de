fde3cfa997fae8edeec572bea1c1fd3043dcc379
Sascha Brink <sascha.brink@gmail.com>
2017-09-27 09:06:56 +0200

    Change hook for events

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -1,4 +1,4 @@
-Jekyll::Hooks.register :site, :pre_render do |site|
+Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
   response = RestClient.get('http://workshops.de/api/events')
   File.write('_includes/_events_jsonld.json', response.body)
