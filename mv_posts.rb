6477d11c14284c1d63c2108a5e256cb554613324
Sascha Brink <sascha.brink@gmail.com>
2018-07-12 23:54:27 +0200

    Move header images

--- mv_posts.rb
+++ mv_posts.rb
@@ -11,3 +11,16 @@ Dir["_posts/*.md"].each do |file|
   File.rename file, new_file
 end
 
+Dir["_posts/**/*.md"].each do |file|
+  name = File.basename(file, ".md")[11..-1]
+
+  header_file = "artikel/header_images/#{name}.jpg"
+  new_header_file = "#{File.dirname(file)}/header.jpg"
+
+  if !File.exist?(new_header_file)
+    puts "Moving header #{name}: #{File.exist?(header_file)}"
+    if File.exist?(header_file)
+      File.rename header_file, new_header_file
+    end
+  end
+end
