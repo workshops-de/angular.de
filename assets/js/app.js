b000ebe9935aa802bccf6bfa1e5a4d449afae8f6
Sascha Brink <sascha.brink@gmail.com>
2018-07-27 12:38:50 +0200

    Readd headroom for navigation

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+var myElement = document.querySelector("[data-headroom]");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init();
