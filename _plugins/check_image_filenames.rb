1b752f1da9af5ee1dd00989f45a3c3d299078a17
Sascha Brink <sascha.brink@gmail.com>
2018-08-16 16:07:42 +0200

    Change testimonial style

--- _plugins/check_image_filenames.rb
+++ _plugins/check_image_filenames.rb
@@ -1,10 +1,19 @@
 Jekyll::Hooks.register :site, :after_init do |p|
   require 'fastimage'
-  puts "Checking header images"
+  puts "Checking image filenames"
   Dir["assets/**/*.jpeg"].each do |file|
     puts "Image: #{file}"
     puts "Don't use extension .jpeg, rename it to .jpg"
     exit 1
   end
-  puts "Checking header images...done"
+
+  Dir["assets/**/*.{jpg,gif,png,svg}"].each do |file|
+    basename = File.basename(file, File.extname(file))
+    if basename !~ /^[a-z0-9-]+$/
+      puts "Image: #{basename}"
+    end
+    # puts "Don't use extension .jpeg, rename it to .jpg"
+    # exit 1
+  end
+  puts "Checking images filenames...done"
 end
