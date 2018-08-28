47b11c82f9c32121e1e80a77140a99b55d725060
Robin Böhm <robin.boehm@workshops.de>
2018-08-28 12:25:49 +0200

    feat(course): fetch only course related events

--- _plugins/fetch_course_reviews.rb
+++ _plugins/fetch_course_reviews.rb
@@ -1,7 +1,7 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
-  puts "Fetching reviews..."
-  response = RestClient.get('http://workshops.de/api/course/4')
+  puts "Fetching course..."
+  response = RestClient.get('https://workshops.de/api/course/4')
   File.write('_data/course.json', response.body)
-  puts "Fetching reviews...done"
+  puts "Fetching course...done"
 end
--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -1,5 +1,5 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
-  response = RestClient.get('http://workshops.de/api/events')
+  response = RestClient.get('https://workshops.de/api/course/4/events')
   File.write('_data/events.json', response.body)
 end
