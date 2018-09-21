8e83e7b6ab0ef3b1ffaec709c8628614dbfc052b
Robin Böhm <robin.boehm@workshops.de>
2018-09-21 21:53:47 +0200

    feat(rating): activate json_ld for reviews

--- _plugins/fetch_course_reviews.rb
+++ _plugins/fetch_course_reviews.rb
@@ -3,5 +3,6 @@ Jekyll::Hooks.register :site, :after_init do |site|
   puts "Fetching course..."
   response = RestClient.get('https://workshops.de/api/course/4')
   File.write('_data/course.json', response.body)
+  File.write('_includes/_course_reviews.json', response.body)
   puts "Fetching course...done"
 end
