d1c202bde81461de503a7e7f1c9a9d7a55eebc8f
Sascha Brink <sascha.brink@gmail.com>
2018-09-25 16:26:40 +0200

    feat(schulungen): fetch events.json for intensiv and advanced

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -1,5 +1,8 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
   response = RestClient.get('https://workshops.de/api/course/4/events')
-  File.write('_data/events.json', response.body)
+  File.write('_data/events/angular-intensiv.json', response.body)
+
+  response = RestClient.get('https://workshops.de/api/course/23/events')
+  File.write('_data/events/angular-advanced.json', response.body)
 end
