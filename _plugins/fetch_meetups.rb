12808741f2453f17b507f55cc7ee1ccc443e23dd
Robin Böhm <robin.boehm@workshops.de>
2019-07-18 14:39:58 +0200

    feat(meetups): add plugin to fetch meetups

---
+++ _plugins/fetch_meetups.rb
@@ -0,0 +1,37 @@
+Jekyll::Hooks.register :site, :after_init do |site|
+  require 'rest-client'
+  puts "Fetching meetups..."
+  filename = '_data/meetups.json'
+
+  meetups = [
+    'AngularJS-Meetup-Berlin',
+    'angular-users-stuttgart',
+    'Angular-Ruhr',
+    'Angular-Meetup-Leipzig',
+    'Angular-Frankfurt',
+    'angular-cologne',
+    'angular-heidelberg',
+    'Angular-Kiel',
+    'Angular-Meetup-Karlsruhe',
+    'Angular-Meetup-Dresden',
+    'Angular-Munich',
+    'Swiss-Angular',
+    'Angular-Vienna',
+    'Hamburg-AngularJS-Meetup',
+    'NgNiederrhein',
+    'Angular-Meetup-Graz',
+    'ngGirls-RheinNeckar',
+  ]
+
+  File.write(filename, '[')
+  meetups.each {
+     |meetup|
+     response = RestClient.get("https://api.meetup.com/#{meetup}?&sign=true&photo-host=public")
+     File.write(filename, response.body, File.size(filename) , mode: 'a')
+     File.write(filename, ',', File.size(filename) , mode: 'a')
+    }
+  File.write(filename, ']',File.size(filename)-1 , mode: 'a')
+
+
+  puts "Fetching meetups...done"
+end
