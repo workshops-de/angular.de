430d71a9a9764863954d8d24c92298572b0921f0
Robin Böhm <robin.boehm@workshops.de>
2019-07-19 18:29:12 +0200

    chore(video): add error message when api key not set

--- _plugins/fetch_videos.rb
+++ _plugins/fetch_videos.rb
@@ -6,7 +6,9 @@ Jekyll::Hooks.register :site, :after_init do |site|
   if(ENV["YOUTUBE_API_KEY"]) then
     response = RestClient.get("https://www.googleapis.com/youtube/v3/playlistItems?part=id%2C%20contentDetails%2C%20snippet%2C%20status&playlistId=PLWn-GjQ4tNVngIY0M1QAUQogyXmaPXtGl&key=#{ENV["YOUTUBE_API_KEY"]}")
     File.write(filename, response.body)
+  else
+    puts "NO ENV YOUTUBE_API_KEY SET - Video fetch aborted"
   end
 
-  puts "Fetching meetups...done"
+  puts "Fetching videos...done"
 end
