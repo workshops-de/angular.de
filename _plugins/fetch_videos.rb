b5bf5352e5fa99fa245475cc86d7675da971d839
Robin Böhm <robin.boehm@workshops.de>
2019-07-19 18:31:00 +0200

    feat(videos): inc maxResults to 50

--- _plugins/fetch_videos.rb
+++ _plugins/fetch_videos.rb
@@ -4,7 +4,7 @@ Jekyll::Hooks.register :site, :after_init do |site|
   filename = '_data/videos.json'
 
   if(ENV["YOUTUBE_API_KEY"]) then
-    response = RestClient.get("https://www.googleapis.com/youtube/v3/playlistItems?part=id%2C%20contentDetails%2C%20snippet%2C%20status&playlistId=PLWn-GjQ4tNVngIY0M1QAUQogyXmaPXtGl&key=#{ENV["YOUTUBE_API_KEY"]}")
+    response = RestClient.get("https://www.googleapis.com/youtube/v3/playlistItems?part=id%2C%20contentDetails%2C%20snippet%2C%20status&playlistId=PLWn-GjQ4tNVngIY0M1QAUQogyXmaPXtGl&maxResults=50&key=#{ENV["YOUTUBE_API_KEY"]}")
     File.write(filename, response.body)
   else
     puts "NO ENV YOUTUBE_API_KEY SET - Video fetch aborted"
