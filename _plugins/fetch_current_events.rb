99bad50a7efbc632b8618bbd2e3435b07434c369
Robin Böhm <robin.boehm@workshops.de>
2019-11-07 17:58:59 +0100

    fix(events): fix event listing for nestjs and rxjs

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -5,4 +5,11 @@ Jekyll::Hooks.register :site, :after_init do |site|
 
   response = RestClient.get('https://workshops.de/api/course/23/events')
   File.write('_data/events/angular-enterprise-applications.json', response.body)
+
+  response = RestClient.get('https://workshops.de/api/course/29/events')
+  File.write('_data/events/nestjs.json', response.body)
+
+  response = RestClient.get('https://workshops.de/api/course/28/events')
+  File.write('_data/events/rxjs.json', response.body)
+  https://workshops.de/api/course/28/events
 end
