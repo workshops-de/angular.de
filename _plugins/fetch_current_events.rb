408d97bc8c13ca818c91bfe59d4edb9a7e6c49a6
Robin Böhm <robin.boehm@workshops.de>
2020-03-24 16:31:04 +0100

    feat(courses): add explicit bookings for remote

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -11,4 +11,7 @@ Jekyll::Hooks.register :site, :after_init do |site|
 
   response = RestClient.get('https://workshops.de/api/course/28/events')
   File.write('_data/events/rxjs.json', response.body)
+
+  response = RestClient.get('https://workshops.de/api/course/35/events')
+  File.write('_data/events/angular-remote.json', response.body)
 end
