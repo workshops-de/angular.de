2e00c750bd9f5c3307165882a0458bc8df239c2b
Robin Böhm <robin.boehm@workshops.de>
2020-04-15 11:34:00 +0200

    feat(events): fetch angular main events for remote listing

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -12,6 +12,6 @@ Jekyll::Hooks.register :site, :after_init do |site|
   response = RestClient.get('https://workshops.de/api/course/28/events')
   File.write('_data/events/rxjs.json', response.body)
 
-  response = RestClient.get('https://workshops.de/api/course/35/events')
+  response = RestClient.get('https://workshops.de/api/course/4/events')
   File.write('_data/events/angular-remote.json', response.body)
 end
