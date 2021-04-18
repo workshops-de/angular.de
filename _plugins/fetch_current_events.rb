0edf50c9112f96727b747871f39a9553c452a227
Sascha <mail@sascha.dev>
2021-04-18 23:00:30 +0200

    feat(course): add html and css course

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -14,4 +14,7 @@ Jekyll::Hooks.register :site, :after_init do |site|
 
   response = RestClient.get('https://workshops.de/api/course/4/events')
   File.write('_data/events/angular-remote.json', response.body)
+
+  response = RestClient.get('https://workshops.de/api/course/26/events')
+  File.write('_data/events/html-css.json', response.body)
 end
